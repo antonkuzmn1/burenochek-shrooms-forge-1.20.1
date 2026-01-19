6e780226460370944751500109e6efac45325968
anton <antonkuzmn1@gmail.com>
2026-01-20 06:43:33 +0700

    ToadstoolItem: added onclick handler

--- src/main/java/com/antonkuzmn1/xqwkeburenochekshrooms/items/ToadstoolItem.java
+++ src/main/java/com/antonkuzmn1/xqwkeburenochekshrooms/items/ToadstoolItem.java
@@ -2,8 +2,15 @@ package com.antonkuzmn1.xqwkeburenochekshrooms.items;
 
 import com.antonkuzmn1.xqwkeburenochekshrooms.client.renderers.ToadstoolItemRenderer;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.BedBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.NotNull;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.AnimatableManager;
@@ -35,5 +42,19 @@ public class ToadstoolItem extends Item implements GeoItem {
                 return new ToadstoolItemRenderer();
             }
         });
+    }
+
+    @Override
+    public @NotNull InteractionResult useOn(@NotNull UseOnContext context) {
+        BlockState state = context.getLevel().getBlockState(context.getClickedPos());
+
+        ResourceLocation targetId = ForgeRegistries.BLOCKS.getKey(state.getBlock());
+
+        if (targetId != null && targetId.toString().equals("minecraft:white_bed")) {
+            System.out.println("Clicked!");
+            return InteractionResult.SUCCESS;
+        }
+
+        return InteractionResult.PASS;
     }
 }
